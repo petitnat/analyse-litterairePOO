4337b71594275dfb091a47470b8e5179adf4d565
ClemDetr <clemence.detroyat@gmail.com>
2026-02-24 10:02:53 +0100

    Update texte.py

---
+++ src/texte.py
@@ -0,0 +1,6 @@
+class Texte :
+    def __init__(self, titre: str, auteur: str, contenu: str, annee: int):
+        self.titre = titre
+        self.auteur = auteur
+        self.contenu = contenu
+        self.annee = annee
